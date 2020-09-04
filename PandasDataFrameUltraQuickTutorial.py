3c5af40bb13b111f2196e01ddb4a3e69950440d0
xvalad <adams.vallejos@gu.se>
2020-09-04 10:20:36 +0200

    Instructions for task1

--- PandasDataFrameUltraQuickTutorial.py
+++ PandasDataFrameUltraQuickTutorial.py
@@ -9,7 +9,7 @@ import pandas as pd
 my_data = np.array([[0,3],[10,7],[20,9],[30,14],[40,15]])
 #
 # Create a Python list that holds the names of the two columns.
-my_column_names = ['temperatures','activity']
+my_column_names = ['temperature','activity']
 #
 # Create a data frame
 my_dataframe = pd.DataFrame(data=my_data, columns=my_column_names)
@@ -30,3 +30,12 @@ print(my_dataframe.iloc[[2]],'\n')
 #
 print("Rows #1, #2, and #3:")
 print(my_dataframe[1:4], '\n')
+#
+print("Column 'temperature':")
+print(my_dataframe['temperature'])
+#
+# Task 1: Create a DataFrame
+# Create a 3x4 pandas DataFrame in which thee columns are named Eleanor, Chidi, Tahani, and Jason.
+# Populate each of the cells in the DataFrame with a random integer between 0 and 100, inclusive.
+# Output the following: - The entire DataFrame
+#                       - The value of the cell of row #1 of the Eleanor column 
