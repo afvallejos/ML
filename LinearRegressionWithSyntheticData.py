baf71a32fe5c5cf8e9f79a7ec46f59b878f87965
xvalad <adams.vallejos@gu.se>
2020-09-04 17:13:06 +0200

    Modified header

--- LinearRegressionWithSyntheticData.py
+++ LinearRegressionWithSyntheticData.py
@@ -1,4 +1,4 @@
-#!/home/adams/xvalad/ML/venv/bin/python
+#!/usr/bin/python3
 # LinearRegressionWIthSyntheticData by Google
 # https://colab.research.google.com/github/google/eng-edu/blob/master/ml/cc/exercises/linear_regression_with_synthetic_data.ipynb
 #
--- PandasDataFrameUltraQuickTutorial.py
+++ PandasDataFrameUltraQuickTutorial.py
@@ -1,4 +1,4 @@
-#!/home/adams/xvalad/ML/venv/bin/python
+#!/usr/bin/python3
 # NumpyUltraQuickTutorial by Google
 # https://colab.research.google.com/github/google/eng-edu/blob/master/ml/cc/exercises/pandas_dataframe_ultraquick_tutorial.ipynb
 #
