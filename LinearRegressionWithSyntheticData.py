b01b05d2462da0f2498b8d632dedf80f6094c804
xvalad <adams.vallejos@gu.se>
2020-09-04 15:24:03 +0200

    Create LinearRegression... script

---
+++ LinearRegressionWithSyntheticData.py
@@ -0,0 +1,4 @@
+#!/usr/bin/python3
+# LinearRegressionWIthSyntheticData by Google
+# https://colab.research.google.com/github/google/eng-edu/blob/master/ml/cc/exercises/linear_regression_with_synthetic_data.ipynb
+#
