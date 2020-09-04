9c5828990858afed6dd0e788f86e01097b1dd04e
xvalad <adams.vallejos@gu.se>
2020-09-04 17:06:46 +0200

    Linear regression file finished

--- PandasDataFrameUltraQuickTutorial.py
+++ PandasDataFrameUltraQuickTutorial.py
@@ -1,4 +1,4 @@
-#!/usr/bin/python3
+#!/home/adams/xvalad/ML/venv/bin/python
 # NumpyUltraQuickTutorial by Google
 # https://colab.research.google.com/github/google/eng-edu/blob/master/ml/cc/exercises/pandas_dataframe_ultraquick_tutorial.ipynb
 #
